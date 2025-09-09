f82da2496a6da41463e27c15e682b62410abae19
alexandremarchant0@gmail.com <alexandremarchant0@gmail.com>
2025-09-09 10:41:01 +0200

    Commits #2

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 'use strict';
 
+/*  *********************** Serveur Web ***************************   */
+// 
+var express = require('express'); 
+var exp = express(); 
+var port = 80;
+exp.use(express.static(__dirname + '/www'));
+
+exp.get('/', function (req, res) {
+    console.log('Reponse a un client'); 
+    res.sendFile(__dirname + '/www/index.html');
+}); 
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+}); 
+
+exp.listen(80, function () {
+    console.log('Serveur en ecoute');
+});
 console.log('TP CIEL');
